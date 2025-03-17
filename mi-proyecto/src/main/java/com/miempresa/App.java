b8c62a1ca9cdeae438e21f688af350c31de5347f
dhurtado07 <informatica.live@gmail.com>
2025-03-17 16:59:51 -0400

    primer proyecto manven y jenkins

---
+++ mi-proyecto/src/main/java/com/miempresa/App.java
@@ -0,0 +1,12 @@
+package com.miempresa;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello maven" );
+    }
+    public int sumar(int a, int b){
+        return a+b;
+    }
+}
